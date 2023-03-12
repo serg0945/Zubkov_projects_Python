8bbe94e8333440f6e28befacbdc93ba372e20b10
serg0945 <97911569+serg0945@users.noreply.github.com>
2023-03-12 23:22:19 +0300

    PZ_16

---
+++ PZ_16/main.py
@@ -0,0 +1,10 @@
+from Figures import circle_perimeter, circle_area, square_perimetr, square_area, triangle_perimetr, triangle_area
+
+#  Я импортировал все функции, чтобы показать, что они реально работают.
+
+print(circle_perimeter())  # Введите одно значение для получения результата
+print(circle_area())  # Введите одно значение для получения результата
+print(square_perimetr())  # Введите одно значение для получения результата
+print(square_area())  # Введите одно значение для получения результата
+print(triangle_perimetr())  # Введите три значения через запятую для получения результата
+print(triangle_area())  # Введите три значения через запятую для получения результата
