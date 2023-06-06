53d5b90924c377a71300ce235f74e1f3237586ab
serg0945 <97911569+serg0945@users.noreply.github.com>
2023-06-06 09:40:20 +0300

    Cowboy Harry

---
+++ Cowboy Harry/assets.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+person_running_1 = pygame.image.load('assets/Dino/Person_run_1.png')
+person_running_2 = pygame.image.load('assets/Dino/Person_run_2.png')
+
+person_jump = pygame.image.load('assets/Dino/Person_attraction.png')
+
+person_shoot_1 = pygame.image.load('assets/Dino/Person_shoot_1.png')
+person_shoot_2 = pygame.image.load('assets/Dino/Person_shoot_2.png')
+
+person_attraction = pygame.image.load('assets/Dino/Person_attraction.png')
+
+small_cactus_1 = pygame.image.load('assets/Cactus/Small_cactus_1.png')
+small_cactus_1_2 = pygame.image.load('assets/Cactus/Small_cactus_1_2.png')
+small_cactus_2 = pygame.image.load('assets/Cactus/Small_cactus_2.png')
+small_cactus_2_2 = pygame.image.load('assets/Cactus/Small_cactus_2_2.png')
+small_cactus_3 = pygame.image.load('assets/Cactus/Small_cactus_3.png')
+small_cactus_3_2 = pygame.image.load('assets/Cactus/Small_cactus_3_2.png')
+
+bird_1 = pygame.image.load('assets/Bird/Bird_1.png')
+bird_2 = pygame.image.load('assets/Bird/Bird_2.png')
+
+track = pygame.image.load('assets/Other/Track.png')
+
+music = pygame.mixer.music.load("assets/music.mp3")
